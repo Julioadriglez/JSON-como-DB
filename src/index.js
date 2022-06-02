cc28838a49068847482442fcd695ddb0defa5cfa
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-02 15:49:43 -0500

    se agrego app.get('port') en index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 
 async function main () {
-    await app.listen(3000);
-    console.log('Server on port 3000')
+    await app.listen(app.get('port'));
+    console.log('Server on port ', app.get('port'));
 }
 main();
 
