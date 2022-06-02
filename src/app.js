68527df97098c9213a52baa15cca8a568b68b07a
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-02 15:51:11 -0500

    agrego set port y set views

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path') //el modulo path nos permite concatenar directorios y hacerlo multiplataforma(si se aplica en linux o mac servira igual)
+
+//Configuración (Settings)
+app.set('port', 5000);
+app.set('views', path.join(__dirname, 'views'));
+
+
+//Middleware
+
+//Routes
+
+//Static
 
 module.exports = app;
