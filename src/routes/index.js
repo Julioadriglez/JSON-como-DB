6c19bed4b469ce560bba3a69b0bb324bfe38b162
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-02 18:23:05 -0500

    se agrego el render para llamar al index.ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.render('index.ejs'); //render manda a llamar al archivo index.ejs para mostrarlo
+});
+
 module.exports = router;
