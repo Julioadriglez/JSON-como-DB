5d3f8ee3a690e1717465ed96b568e7d0737e7e69
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-05 18:49:04 -0500

    se agrgo condicion if

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+const books = [];
+
 router.get('/', (req, res) => {
     res.render('index.ejs'); //render manda a llamar al archivo index.ejs para mostrarlo
 });
@@ -9,8 +11,19 @@ router.get('/new-entry', (req, res) => {
     res.render('new-entry.ejs');
 });
 
-router.post('/new-entry', (req, res) => {
-    console.log(req.body.json);
+router.post('/new-entry', (req, res) => { //Recibe los datos del libro 
+    const {title, imagen, author, description} = req.body;
+    if (title || imagen || author || description){
+        res.status(400).send('Entries must have a title and description');
+        return;
+    }
+    let newbook = {
+        title,
+        imagen,
+        author,
+        description
+    };
+    books.push(req.body);
     res.send('received');
 });
 
