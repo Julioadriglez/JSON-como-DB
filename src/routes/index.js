204d26bfefc82ff780f48c76a9597389d367dd67
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-07 15:55:33 -0500

    endpoint de delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const json_books = fs.readFileSync('src/books.json', 'utf-8',);// Lee el archivo .json
-const books = JSON.parse(json_books);
+let books = JSON.parse(json_books);
 
 router.get('/', (req, res) => {
     res.render('index.ejs', {
@@ -36,6 +36,13 @@ router.post('/new-entry', (req, res) => { //Recibe los datos del libro
 
     res.redirect('/');
 });
+
+router.get('/delete/:id', (req, res) => {
+    books = books.filter(book => book.id != req.params.id); //filtra y elemina el libro que se paresca al id que es mandado por req.params.id  
+    const json_books = JSON.stringify(books) // Se guardan datos en books.json
+    fs.writeFileSync('src/books.json', json_books, 'utf-8'); //Direccion de books.json y formato en como se guarda el string
+    res.redirect('/'); //redirecciona a la ruta inicial 
+})
 
 
 module.exports = router;
